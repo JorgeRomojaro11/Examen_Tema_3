d26b8e259b202d8fd35d2147133eb52f858efe8a
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-05 10:44:30 +0100

    .

--- main.cpp
+++ main.cpp
@@ -9,4 +9,13 @@ public:
     Environment() {
         symbolTable = std::map<std::string, int >();
     }
+
+    Void insert(const std::string& symbol, int value) {
+        if symbolTable.find(symbol) != symbolTable.end() {
+            std::cout << "Symbol " << symbol << " already exists" << std::endl;
+        } else {
+            symbolTable.insert(std::pair<std::string, int>(symbol, value));
+        }
+
+    }
 };
